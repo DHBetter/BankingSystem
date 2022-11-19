15dbeb7ee1084c059f6c04afc5b5c557c79b8c8f
Daniel Ledbetter <dledbe14@uncc.edu>
2022-11-19 14:29:34 -0500

    changing menu and account numbers

--- src/com/bank/Account.java
+++ src/com/bank/Account.java
@@ -1,15 +1,34 @@
 package com.bank;
+//import java.util.*;
 
 import java.util.Random;
 
 public class Account {
-    public int accountNum() {
+    Random random = new Random();
+    private String randomCardFix;
+    private String randomPinFix;
+    private double balance;
 
-        return 0;
+
+    public String cardNum() {
+        System.out.println("");
+        System.out.println("Your card has been created:");
+        System.out.println("Your card number is:");
+        long randomCard = random.nextLong(999999999999999L);
+        randomCardFix = "4" + String.format("%15d", randomCard);
+        balance = 0.0;
+        return randomCardFix;
     }
-    public int pinNum() {
-        Random random = new Random();
-        int number = random.nextInt(9999);
-        return number;
+    public String pinNum() {
+        System.out.println("Your card PIN:");
+
+        int randomPin = random.nextInt(9999);
+        randomPinFix = String.format("%04d", randomPin);
+        return randomPinFix;
+    }
+    public void cardCheck(String cardNum, String pinNum){
+        if (cardNum.equals(randomCardFix) && pinNum.equals(randomPinFix)) {
+                System.out.println("You have successfully logged in!");
+        }
     }
 }
--- src/com/bank/Main.java
+++ src/com/bank/Main.java
@@ -6,27 +6,34 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         menu();
+
+
     }
-    public static String menu(){
+    public static void menu(){
         Scanner scanner = new Scanner(System.in);
         Account account = new Account();
-        System.out.println("1. Create an account\n" +
-                "2. Log into account\n" +
-                "3. Exit");
+        int menuNumber = 0;
+        while(menuNumber != 3) {
+            System.out.println("");
+            System.out.println("1. Create an account\n" +
+                    "2. Log into account\n" +
+                    "3. Exit\n");
 
-        int number = scanner.nextInt();
-        switch(number){
-            case 1:
+            int number = scanner.nextInt();
+            switch (number) {
+                case 1:
+                    System.out.println(account.cardNum());
+                    System.out.println(account.pinNum());
+                    break;
+                case 2:
 
-            case 2:
-                System.out.println(account.pinNum());
-                break;
-            case 3:
-
+                    break;
+                case 3:
+                    menuNumber = 3;
+                    break;
+            }
         }
-        return "dude";
     }
 
 }
